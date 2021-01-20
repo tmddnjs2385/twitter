67dd48f450de733a2600c1f32562634b1a5abccf
OptimusLee <tmddnjs2385@naver.com>
2021-01-20 13:48:42 +0900

    Router Setup

---
+++ src/components/Router.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+
+const AppRouter = () => {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    return (
+        <Router>
+            <Switch>
+
+                {isLoggedIn ?
+                    <>
+                        <Route>
+                            <Home />
+                        </Route>
+                    </> :
+                    <Route>
+                        <Auth />
+                    </Route>}
+
+            </Switch>
+        </Router>
+    )
+
+}
+
+
+export default AppRouter;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import App from './components/App';
 import firebase from "./firebase";
 
 console.log(firebase);
-
 
 ReactDOM.render(
   <React.StrictMode>
