8cd6429f7a74e7dd4af127cbe264b532ed25eae6
OptimusLee <tmddnjs2385@naver.com>
2021-01-20 11:24:29 +0900

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDLz3xE1SvcwMr-Nb8N3h46rfyUOw7mbig",
+    authDomain: "twitter-f79db.firebaseapp.com",
+    projectId: "twitter-f79db",
+    storageBucket: "twitter-f79db.appspot.com",
+    messagingSenderId: "498508042272",
+    appId: "1:498508042272:web:22e17e24004c76d350ec45"
+};
+
+export default firebase.initializeApp(firebaseConfig);
