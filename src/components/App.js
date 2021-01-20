3e4792b290bebbdcbcef09a7e6979c1209d37b4e
OptimusLee <tmddnjs2385@naver.com>
2021-01-20 19:23:46 +0900

    login form part one

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import AppRouter from "./Router";
+import { authService } from "..//fbase";
 
 function App() {
+
+
+
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+
+  console.log(isLoggedIn);
+
   return (
-    <AppRouter />
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} twitter</footer>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App';
-import firebase from "./firebase";
 
-console.log(firebase);
 
 ReactDOM.render(
   <React.StrictMode>
